7384bb79f0aeba0612a95d70cd45094dcaa2e1ba
Edward Santiesteban <santiesteban@outlook.com>
2018-01-09 14:28:04 -0600

    Added firebase push notification integration, default icon to display the push notifications, as well as a mock service to receive the messages when on foreground

---
+++ app/src/main/java/mx/tlacahuepec/ppd_aa/service/MyFirebaseMessagingService.java
@@ -0,0 +1,10 @@
+package mx.tlacahuepec.ppd_aa.service;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by santi on 1/9/2018.
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    //TODO: implementation
+}
